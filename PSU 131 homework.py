b8de13c06eaeac68c30b6b9e97c84d98305a125f
Spluffy <156278452+Spluffy44@users.noreply.github.com>
2024-09-25 11:39:39 -0400

    Update PSU 131 homework.py

--- PSU 131 homework.py
+++ PSU 131 homework.py
@@ -2,8 +2,6 @@ primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67
 factors = []
 
 numbers = int(input('Plug in a number to factor: '))
-a = numbers
-
 
 for each in primes:
     if a % each == 0:
