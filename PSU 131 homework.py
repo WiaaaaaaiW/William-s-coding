cb6a2108d5126e0ca343fb6abfc0d4ba3da85787
Spluffy <156278452+Spluffy44@users.noreply.github.com>
2024-09-25 11:32:50 -0400

    Update PSU 131 homework.py

--- PSU 131 homework.py
+++ PSU 131 homework.py
@@ -1,7 +1,7 @@
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 factors = []
 
-numbers = int(input('please plug in a number'))
+numbers = int(input('Plug in a number to factor: '))
 a = numbers
 s = True
 
